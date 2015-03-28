7123877248dfc607c4d5d295d321daa7ac03f52f
laurajauch <laura.jauch@my.wheaton.edu>
2015-03-28 16:29:52 -0500

    Refine test-error

--- runTests.py
+++ runTests.py
@@ -1,9 +1,12 @@
 from testFunParser import *
 from testSaveLoad import *
+from testRefine import *
 import unittest
 
 testSuite = unittest.makeSuite(testFunParser)
 testSuite.addTest(unittest.makeSuite(testSaveLoad))
+testSuite.addTest(unittest.makeSuit(testRefine))
+#testSuite = unittest.makeSuite(testRefine)
 
 testRunner = unittest.TextTestRunner()
 testRunner.run(testSuite)
