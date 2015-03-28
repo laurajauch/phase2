02d90f5f61696fd9927958517087d372b545d814
laurajauch <laura.jauch@my.wheaton.edu>
2015-03-28 15:52:27 -0500

    test save and load

--- runTests.py
+++ runTests.py
@@ -1,7 +1,9 @@
 from testFunParser import *
+from testSaveLoad import *
 import unittest
 
 testSuite = unittest.makeSuite(testFunParser)
+testSuite.addTest(unittest.makeSuite(testSaveLoad))
 
 testRunner = unittest.TextTestRunner()
 testRunner.run(testSuite)
